f69e6c5880e7ac6148e1cb63d2149466b9b59f07
mlelien <emily.c.lien@gmail.com>
2018-03-18 14:34:19 +0100

    start

---
+++ app/src/main/java/com/example/emilylien/login/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.emilylien.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
